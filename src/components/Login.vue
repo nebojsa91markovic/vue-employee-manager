10a98b9414c8d3fb4d06436fcd2c83a51df1f45a
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2021-02-23 22:09:33 +0100

    auth added

---
+++ src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m8 offset-m2">
+          <div class="login card-panel green white-text center">
+            <h3>Login</h3>
+            <form>
+              <div class="input-field">
+                <i class="material-icons prefix">email</i>
+                <input type="email" id="email" v-model="email" />
+                <label class="white-text" for="email">Email Address</label>
+              </div>
+              <div class="input-field">
+                <i class="material-icons prefix">lock</i>
+                <input type="password" id="password" v-model="password" />
+                <label class="white-text" for="password">Password</label>
+              </div>
+              <button
+                v-on:click="login"
+                class="btn btn-large btn-extended grey lighten-4 black-text"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          user => {
+            console.log(user);
+            alert(`You are logged in as ${this.email}`);
+            this.$router.go({ path: this.$router.path });
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,93 @@ import Dashboard from "@/components/Dashboard";
 import NewEmployee from "@/components/NewEmployee";
 import ViewEmployee from "@/components/ViewEmployee";
 import EditEmployee from "@/components/EditEmployee";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import firebase from "firebase";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: "/",
       name: "Dashboard",
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
       path: "/new",
       name: "NewEmployee",
-      component: NewEmployee
+      component: NewEmployee,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/:employee_id",
       name: "ViewEmployee",
-      component: ViewEmployee
+      component: ViewEmployee,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/edit/:employee_id",
       name: "EditEmployee",
-      component: EditEmployee
+      component: EditEmployee,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    //  IF USER IS NOT LOGGED IN
+    if (!firebase.auth().currentUser) {
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    //  IF USER IS LOGGED IN
+    if (firebase.auth().currentUser) {
+      next({
+        path: "/",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
